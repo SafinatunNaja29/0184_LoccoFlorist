dd4faf8e83e98811c74a5a5162a917ff9d3b4836
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-07-03 11:36:42 +0700

    Membuat widget CustomTextField

---
+++ lib/core/components/custom_text_field.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String validator;
+  final TextEditingController controller;
+  final String label;
+  final Function(String value)? onChanged;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final bool showLabel;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool readOnly;
+  final int maxLines;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.label,
+    this.onChanged,
+    this.obscureText = false,
+    this.keyboardType,
+    this.showLabel = true,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.readOnly = false,
+    this.maxLines = 1,
+    required this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (showLabel) ...[
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: AppColors.black,
+            ),
+          ),
+          const SizedBox(height: 8),
+        ],
+        TextFormField(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (vl) {
+            if (vl == null || vl.isEmpty) {
+              return validator;
+            }
+            return null;
+          },
+          controller: controller,
+          onChanged: onChanged,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          readOnly: readOnly,
+          maxLines: maxLines,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon,
+            hintText: label,
+            filled: true,
+            fillColor: AppColors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(color: AppColors.grey),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(color: AppColors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(color: AppColors.pinkFanta, width: 2),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
