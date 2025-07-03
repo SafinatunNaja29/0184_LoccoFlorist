4fcc1b26cde1abdd8cf7f05496545cc0c1fe6b84
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-07-03 11:24:06 +0700

    Menambahkan warna-warna ke AppColors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color pinkPastel = Color(0xFFFFC1CC);
+
+  static const Color purplePastel = Color(0xFFE1C6FF);
+
+  static const Color pinkFanta = Color(0xFFFF3FA4);
+
+  static const Color tosca = Color(0xFF40E0D0);
+
+  static const Color yellowPastel = Color(0xFFFFF9C4);
+
+  static const Color white = Color(0xFFFFFFFF);
+  
+  static const Color black = Color(0xFF000000);
+}
