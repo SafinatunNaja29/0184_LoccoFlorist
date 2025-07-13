80c5894952524b0b7b69fbbef0cd930ae42ee9b3
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-07-14 01:03:31 +0700

    memperbaiki parsing nilai null

--- lib/data/model/produk_model.dart
+++ lib/data/model/produk_model.dart
@@ -23,13 +23,13 @@ class Produk {
 
   factory Produk.fromMap(Map<String, dynamic> map) {
     return Produk(
-      idProduk: map['id_produk'],
+      idProduk: int.tryParse(map['id_produk'].toString()) ?? 0,
       namaProduk: map['nama_produk'],
       deskripsiProduk: map['deskripsi_produk'],
-      hargaProduk: map['harga_produk'],
-      stokProduk: map['stok_produk'],
+      hargaProduk: int.tryParse(map['harga_produk'].toString()) ?? 0,
+      stokProduk: int.tryParse(map['stok_produk'].toString()) ?? 0,
       gambarProduk: map['gambar_produk'],
-      idKategori: map['id_kategori'],
+      idKategori: int.tryParse(map['id_kategori'].toString()) ?? 0,
       produkCreatedAt: DateTime.parse(map['produk_created_at']),
       produkUpdatedAt: DateTime.parse(map['produk_updated_at']),
     );
